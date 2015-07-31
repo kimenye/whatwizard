28b914eecd6f6fa08fc2e1f5f3365896383ae8f8
Trevor <kimenye@gmail.com>
2015-07-31 15:57:10 +0300

    Fixed starter.sh

--- starter.sh
+++ starter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#[ -f "tmp/pids/server.pid" ] && kill -9 `cat "tmp/pids/server.pid`"
+#[ -f "tmp/pids/server.pid" ] && kill -9 `cat "tmp/pids/server.pid"`
 rails server -p 8080 -e production -d
